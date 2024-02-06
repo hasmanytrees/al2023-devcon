acba0c81b86735516e17cbc12e3d2d03b77c59be
Forrest Robertson <forrest@hasmanytrees.com>
2024-02-06 13:36:04 -0700

    Initial revision

---
+++ src/awscli/install.sh
@@ -0,0 +1,1 @@
+yum update && yum -y install awscli-2
---
+++ src/go/install.sh
@@ -0,0 +1,4 @@
+yum update && yum -y install go
+go install -v golang.org/x/tools/gopls@latest
+go install github.com/go-delve/delve/cmd/dlv@latest
+go install honnef.co/go/tools/cmd/staticcheck@latest
